3cdb641a279667c7a22bddca5ccf5040b0bb1a61
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2025-06-04 20:32:42 -0400

    Revert "Add proxy trust and callback env"

--- server/index.ts
+++ server/index.ts
@@ -4,8 +4,6 @@ import { setupVite, serveStatic, log } from "./vite";
 import { setupAuth } from "./auth";
 
 const app = express();
-// behind a proxy (e.g. Replit) use X-Forwarded-Proto for req.protocol
-app.set('trust proxy', 1);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 setupAuth(app);
